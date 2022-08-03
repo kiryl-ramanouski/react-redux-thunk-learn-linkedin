201e964ac2b008d0dde7f069a7a48d2db3310c59
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 12:04:28 +0300

    fix: fix App.css import

--- REACT-ECOSYSTEMS-APP/src/App.js
+++ REACT-ECOSYSTEMS-APP/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 // Components
 import TodoList from './todos/TodoList';
 
+// Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 // Data
 const todosData = [
