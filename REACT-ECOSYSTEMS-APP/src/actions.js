f0f948c157bd962cb855b39ac65814163cba054f
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 18:43:23 +0300

    fix: fix actions-creators fields. Must be {type: ACTION, payload: { parameters }, }

--- REACT-ECOSYSTEMS-APP/src/actions.js
+++ REACT-ECOSYSTEMS-APP/src/actions.js
@@ -1,10 +1,11 @@
 export const CREATE_TODO = 'CREATE_TODO';
 export const createTodo = (text) => ({
-  action: CREATE_TODO,
-  payload: text,
+  type: CREATE_TODO,
+  payload: { text },
 });
+
 export const REMOVE_TODO = 'REMOVE_TODO';
 export const removeTodo = (text) => ({
-  action: REMOVE_TODO,
-  payload: text,
+  type: REMOVE_TODO,
+  payload: { text },
 });
