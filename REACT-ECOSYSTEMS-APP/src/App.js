2803513803fd55ba4235757498c017d5b06b0211
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 11:43:09 +0300

    feat: import bootstrap, add simple data, add TodoList to App

--- REACT-ECOSYSTEMS-APP/src/App.js
+++ REACT-ECOSYSTEMS-APP/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// Components
+import TodoList from './todos/TodoList';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Data
+const todosData = [
+  { text: 'Make a cake' },
+  { text: 'Make a cake' },
+  { text: 'Make a cake' },
+];
+
 const App = () => {
   return (
     <div className='App'>
-      <h1>Hello, World!</h1>
+      <TodoList todos={todosData} />
     </div>
   );
 };
