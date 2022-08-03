9ab5f3d7ef0e5360aec41014855d41a0a1a43612
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 16:59:54 +0300

    feat: add redux to application. Use <Provider>, store={configureStore}

--- REACT-ECOSYSTEMS-APP/src/index.js
+++ REACT-ECOSYSTEMS-APP/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 
+// Redux
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+
 // Components
 import App from './App';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={configureStore}>
+    <App />
+  </Provider>
+);
