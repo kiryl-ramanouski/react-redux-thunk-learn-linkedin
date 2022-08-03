b161933266c765447cdaeefba1bd722d64d26c8e
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 18:50:13 +0300

    feat: connect TodoList to react: use export default connect(mapStateToProps, mapDispatchToProps)(TodoList);

--- REACT-ECOSYSTEMS-APP/src/todos/TodoList.js
+++ REACT-ECOSYSTEMS-APP/src/todos/TodoList.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Redux
+import { connect } from 'react-redux';
+import { removeTodo } from '../actions';
+
 // React-bootstrap components
 import ListGroup from 'react-bootstrap/ListGroup';
 
@@ -7,18 +11,29 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos = [], onRemovePressed }) => {
   return (
     <div className='d-flex flex-column mb-3'>
       <h1 className='text-center'>Todo List</h1>
       <NewTodoForm />
       <ListGroup className='border border-2'>
         {todos.map((todo, i) => {
-          return <TodoListItem key={i} todo={todo} />;
+          return (
+            <TodoListItem
+              key={i}
+              todo={todo}
+              onRemovePressed={onRemovePressed}
+            />
+          );
         })}
       </ListGroup>
     </div>
   );
 };
 
-export default TodoList;
+const mapStateToProps = (state) => ({ todos: state.todos });
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
