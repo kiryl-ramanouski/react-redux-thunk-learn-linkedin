d85642052863485403c1c17d95f81a5a4193785f
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-03 12:39:36 +0300

    feat: add NewTodoForm component to TodoList. Add proper styles for TodoList

--- REACT-ECOSYSTEMS-APP/src/todos/TodoList.js
+++ REACT-ECOSYSTEMS-APP/src/todos/TodoList.js
@@ -5,14 +5,19 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 // Components
 import TodoListItem from './TodoListItem';
+import NewTodoForm from './NewTodoForm';
 
 const TodoList = ({ todos }) => {
   return (
-    <ListGroup>
-      {todos.map((todo, i) => {
-        return <TodoListItem key={i} todo={todo} />;
-      })}
-    </ListGroup>
+    <div className='d-flex flex-column mb-3'>
+      <h1 className='text-center'>Todo List</h1>
+      <NewTodoForm className='mb-3' />
+      <ListGroup className='border border-2'>
+        {todos.map((todo, i) => {
+          return <TodoListItem key={i} todo={todo} />;
+        })}
+      </ListGroup>
+    </div>
   );
 };
 
