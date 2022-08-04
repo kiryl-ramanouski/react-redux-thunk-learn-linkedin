d683eb35f9e4e77d6857ea96797b468e226c7af2
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-04 11:37:45 +0300

    feat: wrap APP into PersistGate and connect it to the redux-persist. Use persistStore, PersistGate,

--- REACT-ECOSYSTEMS-APP/src/index.js
+++ REACT-ECOSYSTEMS-APP/src/index.js
@@ -5,12 +5,21 @@ import ReactDom from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 
+// Redux-persist
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
 // Components
 import App from './App';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
