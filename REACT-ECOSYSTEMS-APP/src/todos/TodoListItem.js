e31aea3213d78b357277cf195ccf539e794d49c3
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-04 13:18:15 +0300

    feat: add onCompletedPressed function to onClick of TodoListItem

--- REACT-ECOSYSTEMS-APP/src/todos/TodoListItem.js
+++ REACT-ECOSYSTEMS-APP/src/todos/TodoListItem.js
@@ -3,15 +3,21 @@ import React from 'react';
 // React-bootstrap components
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const TodoListItem = ({ todo, onRemovePressed }) => {
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
   return (
     <ListGroup.Item className='d-flex justify-content-between'>
       <h3>{todo.text}</h3>
       <ButtonGroup size='sm'>
-        <Button variant='success'>Completed</Button>
+        {todo.isCompleted ? null : (
+          <Button
+            onClick={() => onCompletedPressed(todo.text)}
+            variant='success'
+          >
+            Completed
+          </Button>
+        )}
         <Button onClick={() => onRemovePressed(todo.text)} variant='danger'>
           Remove
         </Button>
