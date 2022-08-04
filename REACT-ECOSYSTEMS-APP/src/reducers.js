328d4d74f60d7b5819180ebd80c19f50155ab6c4
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-04 12:41:05 +0300

    feat: add reducer for COMPLETE_TODO action

--- REACT-ECOSYSTEMS-APP/src/reducers.js
+++ REACT-ECOSYSTEMS-APP/src/reducers.js
@@ -1,4 +1,4 @@
-import { CREATE_TODO, REMOVE_TODO } from './actions';
+import { CREATE_TODO, REMOVE_TODO, COMPLETE_TODO } from './actions';
 
 export const todos = (state = [], action) => {
   const { type, payload } = action;
@@ -15,6 +15,14 @@ export const todos = (state = [], action) => {
     case REMOVE_TODO: {
       const { text } = payload;
       return state.filter((todo) => todo.text !== text);
+    }
+    case COMPLETE_TODO: {
+      const { text } = payload;
+      return state.map((item) => {
+        if (item.text == text) {
+          item.isCompleted = true;
+        }
+      });
     }
     default:
       return state;
