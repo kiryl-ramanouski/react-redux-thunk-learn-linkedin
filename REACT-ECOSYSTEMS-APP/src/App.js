3193722561f81b9ae4f58b39b8a5210f00676eae
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-08 18:12:04 +0300

    feat: use styled-components inside App

--- REACT-ECOSYSTEMS-APP/src/App.js
+++ REACT-ECOSYSTEMS-APP/src/App.js
@@ -6,11 +6,18 @@ import TodoList from './todos/TodoList';
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Styled Components
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  margin: 1rem;
+`;
+
 const App = () => {
   return (
-    <div className='App'>
+    <AppContainer>
       <TodoList />
-    </div>
+    </AppContainer>
   );
 };
 
