3ea690946189ed843ee7a8c3ae726fbb5440fda3
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-08 18:52:49 +0300

    feat: add styled TodoItemWarningBorder component. Use it with props

--- REACT-ECOSYSTEMS-APP/src/todos/TodoListItem.js
+++ REACT-ECOSYSTEMS-APP/src/todos/TodoListItem.js
@@ -5,21 +5,39 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+// Styled Components
+import styled from 'styled-components';
+const TodoItemWarningBorder = styled.div`
+  border-bottom: ${(props) =>
+    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+      ? 'none'
+      : '2px solid red'};
+`;
+
 const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
   return (
-    <ListGroup.Item className='d-flex justify-content-between'>
-      <h3>{todo.text}</h3>
-      <ButtonGroup size='sm'>
-        {todo.isCompleted ? null : (
-          <Button onClick={() => onCompletedPressed(todo.id)} variant='success'>
-            Completed
+    <TodoItemWarningBorder createdAt={todo.createdAt}>
+      <ListGroup.Item className='d-flex justify-content-between align-items-center'>
+        <div>
+          <h3>{todo.text}</h3>
+          <p>Created at: {new Date(todo.createdAt).toLocaleDateString()}</p>
+        </div>
+
+        <ButtonGroup size='sm'>
+          {todo.isCompleted ? null : (
+            <Button
+              onClick={() => onCompletedPressed(todo.id)}
+              variant='success'
+            >
+              Completed
+            </Button>
+          )}
+          <Button onClick={() => onRemovePressed(todo.id)} variant='danger'>
+            Remove
           </Button>
-        )}
-        <Button onClick={() => onRemovePressed(todo.id)} variant='danger'>
-          Remove
-        </Button>
-      </ButtonGroup>
-    </ListGroup.Item>
+        </ButtonGroup>
+      </ListGroup.Item>
+    </TodoItemWarningBorder>
   );
 };
 
