6cd11a368b748fccbad4494b0c67a004b8f91909
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-08 18:08:22 +0300

    refactor: delete temp data and app.css

--- REACT-ECOSYSTEMS-APP/src/App.js
+++ REACT-ECOSYSTEMS-APP/src/App.js
@@ -5,19 +5,11 @@ import TodoList from './todos/TodoList';
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-// Data
-const todosData = [
-  { text: 'Make a cake' },
-  { text: 'Make a cake' },
-  { text: 'Make a cake' },
-];
 
 const App = () => {
   return (
     <div className='App'>
-      <TodoList todos={todosData} />
+      <TodoList />
     </div>
   );
 };
